33fe5ec9b7b5de8490dac9e77af6eb3d7b667f1d
IOANNIS ARONIS <ioannis.aronis1@gmail.com>
2021-09-26 23:13:18 +0000

    Added the division function but still is not complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,8 @@ document.addEventListener("DOMContentLoaded", function() {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2); 
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2); 
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -89,6 +91,8 @@ runGame(calculatedAnswer[1]);
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -131,6 +135,8 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "x";
 }
 
-function displayDivideQuestion() {
-    
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
