926b61c8d76d4bc17ed5970a62c29d2bf7a73db3
IOANNIS ARONIS <ioannis.aronis1@gmail.com>
2021-09-26 19:13:06 +0000

    Display addition question to the user!

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,13 @@ function runGame() {
     //Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gameType === "Addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -41,7 +48,10 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
     
 }
 
